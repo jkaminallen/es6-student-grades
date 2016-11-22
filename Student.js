c49887491ba597c3bf7a9f5360e8046f2f92d1ae
jkaminallen <kaminallenj@gmail.com>
2016-11-22 11:55:18 -0500

    uploading

---
+++ Student.js
@@ -0,0 +1,30 @@
+// define your student class, here
+class Student {
+  constructor(name, grades) {
+    this.name = name;
+    this.grades = grades;
+  }
+
+  averageGrade() {
+    let sum = 0
+    for (let grade of this.grades) {
+      sum += grade
+    }
+      let average = sum / this.grades.length;
+      return average
+    }
+
+  toString() {
+    return `${this.name} has an average grade of ${this.averageGrade()}`
+  }
+}
+
+
+// create a new student object
+let james = new Student ("James", [80, 75.3, 90, 88, 70]);
+
+// print the average grade to the console
+console.log(james.averageGrade());
+
+// print the string representation of the student object
+console.log(james.toString());
